6bc23d304ef735172251c59dd9577a7553f000c3
roynaldi19 <roynaldi19@gmail.com>
2021-02-19 19:29:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bfaa2_02viewconstraint/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bfaa2_02viewconstraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
